eadb02306481e3228ed99d433914beaeb3c4a39d
Jen <lopzjcan@gmail.com>
2020-06-17 19:10:21 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const menu = document.querySelector (".burger");
+const nav = document.querySelector (".nav-links");
+const navLinks = document.querySelectorAll (".nav-links li");
+var open = false;
+
+const navSlide = () => {
+    nav.classList.toggle('nav-active');
+    if (!open) {open = true; nav.classList.add("transition"); }
+    else {
+      open = false;
+      setTimeout(function(){
+        nav.classList.remove('transition')
+      }, 550);
+    }
+    navLinks.forEach((link, i) => {
+      if (link.style.animation) {link.style.animation = "";}
+      else {link.style.animation = `navLinkFade 0.5s ease forwards ${i/7 + .5}s`;}
+
+      link.addEventListener("click", function() {navSlide();})
+    });
+    menu.classList.toggle('toggle');
+    document.querySelector ("#wrap").classList.toggle("fixed");
+}
+
+const touchBurger = () => {
+  menu.addEventListener("click", () => {navSlide();}
+)}
+
+const touchOutside = () => {
+  document.querySelector('#main-img').addEventListener("click", () => {
+    if (open) {navSlide();} });
+}
+
+const accordion = () => {
+  var accordionButtons = document.querySelectorAll('.expand');
+  accordionButtons.forEach((button, i) => {
+    button.addEventListener("click", function() {
+      var content = this.nextElementSibling.nextElementSibling;
+      if (content.style.maxHeight) { content.style.maxHeight = null;  this.innerText = "[+]";}
+      else {content.style.maxHeight = content.scrollHeight + "px"; this.innerText = "[-]";}
+    })
+  });
+}
+
+touchBurger(); touchOutside(); accordion();
